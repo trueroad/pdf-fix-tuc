6a3e1b33a3fc629e647f5fd12d45c25d74d03ee3
Masamichi Hosoda <trueroad@trueroad.jp>
2021-01-27 19:00:31 +0900

    Add some modes to .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,12 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((c++-mode
-  (c-file-style . "gnu")
+((c++-mode .
+  ((c-file-style . "gnu")
   (indent-tabs-mode . nil)))
+ (autoconf-mode .
+  ((indent-tabs-mode . nil)))
+ (python-mode .
+  ((indent-tabs-mode . nil)))
+ (shellscript-mode .
+  ((indent-tabs-mode . nil))))
